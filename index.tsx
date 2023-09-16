017905376c7233680f92dded34dadd6e928deaae
Kosgkagiannis <kosgkagiannis@gmail.com>
2023-09-16 18:53:22 +0200

    Update index location

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // You can create this CSS file for styling
-import App from './App.tsx';
+import App from './src/App.tsx';
 
 ReactDOM.render(
   <React.StrictMode>
