5adf8294ded23252cc3df61d5a69014be9ce2575
Kosgkagiannis <kosgkagiannis@gmail.com>
2023-10-09 21:16:06 +0200

    Header import

--- src/components/GlobalComponents/Header.tsx
+++ src/components/GlobalComponents/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { auth } from "../../config/firebase"
-import HeaderProps from "../../types/HeaderProps"
+import HeaderProps from "../../types/GlobalTypes/HeaderProps"
 
 function Header({ user, handleLogout }: HeaderProps) {
   const navigate = useNavigate()
