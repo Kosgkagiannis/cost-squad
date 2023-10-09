ac05b5fee31f2b7a30367c2501540f2950808530
Kosgkagiannis <kosgkagiannis@gmail.com>
2023-10-09 20:49:54 +0200

    auth update

--- src/components/GlobalComponents/auth.tsx
+++ src/components/GlobalComponents/auth.tsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import { auth, googleProvider } from "../config/firebase";
+import React from "react"
+import { auth, googleProvider } from "../../config/firebase"
 import {
   createUserWithEmailAndPassword,
   signInWithPopup,
   signOut,
-} from "firebase/auth";
-import { useState } from "react";
+} from "firebase/auth"
+import { useState } from "react"
 
 export const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   const signIn = async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      await createUserWithEmailAndPassword(auth, email, password)
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
+  }
 
   const signInWithGoogle = async () => {
     try {
-      await signInWithPopup(auth, googleProvider);
+      await signInWithPopup(auth, googleProvider)
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
+  }
 
   const logout = async () => {
     try {
-      await signOut(auth);
+      await signOut(auth)
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
+  }
 
   return (
     <div>
@@ -52,5 +52,5 @@ export const Auth = () => {
 
       <button onClick={logout}> Logout </button>
     </div>
-  );
-};
+  )
+}
